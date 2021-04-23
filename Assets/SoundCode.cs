86935a3b5c471586ba12ac94cd6a3c06253a3fa6
jhabaa <jhabaa@etu.he2b.be>
2021-04-23 20:23:27 +0200

    Initial commit

---
+++ Assets/SoundCode.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SoundCode : MonoBehaviour
+{
+    public AudioSource source;
+    float[] clipSampleData = new float[1024];
+    bool isSpeaking = false;
+    public float minimumLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        source.clip = Microphone.Start(Microphone.devices[0], true, 10, AudioSettings.outputSampleRate);
+        source.loop = true;
+        while(!(Microphone.GetPosition(null) > 0)) 
+        {
+
+        }
+        source.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        source.GetSpectrumData(clipSampleData, 0, FFTWindow.Rectangular);
+        float currentAverageVolume = clipSampleData.Average();
+        if (currentAverageVolume > minimumLevel)
+        {
+            isSpeaking = true;
+            Debug.Log("BLowing");
+        }
+    }
+}
